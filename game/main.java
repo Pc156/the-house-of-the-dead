ea7fc3714653fd818de9992961fa7e78024d808a
Pc156 <cprathmesh314159@gmail.com>
2024-06-01 12:46:44 +0530

    modified

--- game/main.java
+++ game/main.java
@@ -5,5 +5,7 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("test run for the game ");
+
+        System.out.println("hello to the new world ");
     }
 }
